1061e8b7566e3c864ec802992f36941985d5bbe3
Owen Lovett <ollovett@cpp.edu>
2022-10-07 17:48:25 -0700

    Create ticTacToe.c and add comments

---
+++ ticTacToe.c
@@ -0,0 +1,59 @@
+/*
+
+declare functions
+
+declare variables
+
+main{
+    welcome, ask if pvp or play against computer
+
+    if pvp{
+        say that player 1 is x, player 2 is o
+
+        print board with spaces labeled 1-9 so the user knows which space is what
+
+        clear board
+
+        print game board
+
+        ask player 1 for move (1-9)
+
+        check if move is valid
+            if not valid keep asking until valid
+
+        ask player 2 for move
+
+        check valid
+
+        check for win
+            if no win, loop
+            if win then ask if want to play again
+                if yes then start over
+    }
+    if comp{
+        say that player 1 is x, comp is o
+
+        print board with spaces labeled 1-9 so the user knows which space is what
+
+        clear board
+
+        print game board
+
+        ask player 1 for move (1-9)
+
+        check if move is valid
+            if not valid keep asking until valid
+
+        randomize comp move
+
+        check valid
+            if not, keep randomizing until valid
+
+        check for win
+            if no win, loop
+            if win then ask if want to play again
+                if yes then start over
+    }
+}
+
+*/
